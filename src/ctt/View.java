fb29672a8bc8537ce87923c7f9d6719178c1ee78
milindoka <oak444@gmail.com>
2017-08-09 10:51:32 +0530

    Added Master Table

--- src/ctt/View.java
+++ src/ctt/View.java
@@ -12,18 +12,44 @@ public class View {
 
     
     public View(String text){
-        frame = new JFrame("View");                                    
+        frame = new JFrame("View");                  
+        
+       ////// Create JTable ---------------------------
+        
+        Object rowData[][] = { { "Row1-Column1", "Row1-Column2", "Row1-Column3" },
+                { "Row2-Column1", "Row2-Column2", "Row2-Column3" } };
+            Object columnNames[] = { "Column One", "Column Two", "Column Three" };
+            JTable Master = new JTable(rowData, columnNames);
+
+            JScrollPane scrollPane = new JScrollPane(Master);
+            
+        ///----------------------------------------------
+            
+            
         frame.getContentPane().setLayout(new BorderLayout());                                          
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);           
-        frame.setSize(200,200);
+        frame.setSize(400,400);
         frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        
+        
+        
+        
+        JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+        
         
         label = new JLabel(text);
-        frame.getContentPane().add(label, BorderLayout.CENTER);
+     //   frame.getContentPane().add(label, BorderLayout.CENTER);
+        panel.add(label, BorderLayout.SOUTH);
         
         button = new JButton("Button");        
-        frame.getContentPane().add(button, BorderLayout.SOUTH);        
+        panel.add(button, BorderLayout.NORTH);
+        
+        frame.add(scrollPane, BorderLayout.WEST);
+        frame.add(panel, BorderLayout.EAST);
+        
+        frame.setVisible(true);
+        
     }
         
     public JButton getButton(){
