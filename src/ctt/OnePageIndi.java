902aa16c0fb31bf46da861e9538b17e205f8e3ce
milindoka <oak445@gmail.com>
2018-01-06 18:58:27 +0530

    Individual Print, PageSetup In process

--- src/ctt/OnePageIndi.java
+++ src/ctt/OnePageIndi.java
@@ -11,25 +11,35 @@ public class OnePageIndi
 	public void setView(View vu)  {	this.view=vu; }
 	
 	void PrintOnePage(int topleftx, int toplefty,Graphics g,int pageno)
-	{int  tlx=topleftx, tly=toplefty;
-		DrawSideWallTextLine(tlx,tly,g,0);
+	{
+		int  tlx=topleftx, tly=toplefty;
+		int horizontalwidth=timecolsize+6*othercolsize;
+		
+		g.drawLine(tlx,tly,tlx+horizontalwidth,tly); /////// top line
+	
+		DrawWallTextLine(tlx,tly,g,0);
+		tly+=cellheight;
+		
+		g.drawLine(tlx,tly,tlx+horizontalwidth,tly); /////// top line
 	}
 	 
 	
-	private void DrawSideWallTextLine(int x,int y,Graphics g,int TableRowNo)
+	private void DrawWallTextLine(int x,int y,Graphics g,int TableRowNo)
 	{ 
-	  int currentleft=x,currenttop=y;
-	  
-	 int horizontalwidth=timecolsize+6*othercolsize;
-	    
+	 int currentleft=x,currenttop=y;
+	 
+	 
+	 g.drawLine(currentleft, currenttop, currentleft, currenttop+cellheight); //leftmost wall   
      String  temp=view.GetData(view.table2, TableRowNo,0);
-     DrawSideWallText(temp,currentleft,currenttop, timecolsize, cellheight, g);
+     
+     DrawRightWallText(temp,currentleft,currenttop, timecolsize, g);         /// time with right wall
+     
      currentleft+=timecolsize;
 		        
 		  for(int i=1;i<7;i++) 
 		  { //g.drawString("| Test", currentleft, currenttop);
 			temp=view.GetData(view.table2,TableRowNo,i);
-		    DrawSideWallText(temp,currentleft,currenttop, othercolsize, cellheight, g);
+		    DrawRightWallText(temp,currentleft,currenttop, othercolsize,g);  /// week text with right wall
 		    currentleft+=othercolsize;
 		  }
 	  currentleft=x;
@@ -37,19 +47,18 @@ public class OnePageIndi
 	  currenttop+=cellheight;
 	  }
 	 
-      //g.drawLine(currentleft,currenttop,currentleft+horizontalwidth,currenttop);
 	
 	 
-	 void DrawSideWallText(String str,int tlx,int tly, int boxwidth, int boxheight, Graphics pg)
+	 void DrawRightWallText(String str,int tlx,int tly, int boxwidth,Graphics pg)
 	 {
-		 pg.drawLine(tlx, tly, tlx, tly+boxheight);
-		 pg.drawLine(tlx+boxwidth, tly, tlx+boxwidth, tly+boxheight);
-	//	 pg.fillRect(tlx, tly,boxwidth, boxheight);
+	//	 pg.drawLine(tlx, tly, tlx, tly+cellheight);
+		 pg.drawLine(tlx+boxwidth, tly, tlx+boxwidth, tly+cellheight);
+	//	 pg.fillRect(tlx, tly,boxwidth, cellheight);
 		 int stringLen = (int)  pg.getFontMetrics().getStringBounds(str, pg).getWidth(); 
 		 int stringHeight=(int) pg.getFontMetrics().getStringBounds(str, pg).getHeight();
 		 
 	        int start = boxwidth/2 - stringLen/2;  
-	        pg.drawString(str, start + tlx, tly+(boxheight-stringHeight)/2 +stringHeight-2);
+	        pg.drawString(str, start + tlx, tly+(cellheight-stringHeight)/2 +stringHeight-2);
 	        
 	 }
 	 
