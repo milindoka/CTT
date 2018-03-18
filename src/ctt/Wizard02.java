b25902edf12dba847bc33962e264dba9f881c022
Milind Oka <oak444@gmail.com>
2018-03-18 09:56:17 +0530

    Started Designing Wazard2 Dialog, Main Pane - Basic  Border Layout

--- src/ctt/Wizard02.java
+++ src/ctt/Wizard02.java
@@ -1,11 +1,14 @@
 package ctt;
 
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
  
 
@@ -15,37 +18,47 @@ public String sName;
 
 public Wizard02() 
 {
-setBounds(100, 100, 296, 175);
-setTitle("Input Dialog");
+setBounds(0,0 , 500, 275);
+setTitle("Wizard - Step 2");
 setLocationRelativeTo(null);
-getContentPane().setLayout(null);
-// Create Input
-final JTextField name = new JTextField();
-name.setBounds(57, 36, 175, 20);
-getContentPane().add(name);
- 
+Container dlgpane=getContentPane();
+dlgpane.setLayout(new BorderLayout());
+JLabel label0 = new JLabel("Test");
+JLabel label1 = new JLabel("Test",JLabel.CENTER);
+JLabel label2 = new JLabel("Test",JLabel.CENTER);
+JLabel label4 = new JLabel("Test",JLabel.CENTER);
+JLabel label3 = new JLabel("Test");
+final JTextField name = new JTextField(5);
+//name.setBounds(57, 36, 175, 20);
+dlgpane.add(label0,BorderLayout.WEST);
+//dlgpane.add(name,BorderLayout.CENTER);
+dlgpane.add(label1,BorderLayout.NORTH);
+dlgpane.add(label2,BorderLayout.CENTER);
+dlgpane.add(label3,BorderLayout.EAST);
+dlgpane.add(label4,BorderLayout.SOUTH);
 // Button OK
 JButton btnOK = new JButton("OK");
-btnOK.addActionListener(new ActionListener() {
+btnOK.addActionListener(new ActionListener() 
+{
 public void actionPerformed(ActionEvent e) {
 sName = name.getText();
 dispose();
 }
 });
 
-btnOK.setBounds(70, 93, 78, 23);
-getContentPane().add(btnOK);
+//btnOK.setBounds(70, 93, 78, 23);
+//dlgpane.add(btnOK,BorderLayout.SOUTH);
  
 // Button Cancel
 JButton btnCancel = new JButton("Cancel");
 btnCancel.addActionListener(new ActionListener() {
-public void actionPerformed(ActionEvent e) {
+public void actionPerformed(ActionEvent e) 
+{
 sName = "";
 dispose();
 }
 });
-btnCancel.setBounds(158, 93, 74, 23);
-getContentPane().add(btnCancel);
+//getContentPane().add(btnCancel);
  
 }
 }
