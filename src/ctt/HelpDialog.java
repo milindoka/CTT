a03db9a965bb6ed6af96dbe19e727f117602f29c
Milind Oka <oak444@gmail.com>
2018-04-16 08:52:42 +0530

    Help First Pane Complete

--- src/ctt/HelpDialog.java
+++ src/ctt/HelpDialog.java
@@ -24,9 +24,26 @@ public HelpDialog()
     JPanel p6=new JPanel();  
     
     String text="<H4 align=center>College Time Table CTT is a Free" +
-    		" and Open Source Software</H4><P align=justify p style=margin-left:10px>College Time Table is a" +
-    		"Free and Open Source Software to Generate, Edit, Modify and Manage School / College Time" +
+    		" and Open Source Software</H4><P align=justify p style=margin:10px>College Time Table is a" +
+    		"Free and Open Source Software to Generate, Edit, Modify and Manage School/College Time" +
     		"Table.</P>";
+    
+    text+="<H4 align=center>-- Fetures --" +
+	"</H4>" +
+	"  <ul>"+
+"<li> Simple, Fast, Accurate. Extremly small size and Portable App.</li>"+
+"<li> Starts generating time table instantly and on-the-fly.</li>"+
+"<li> Detects clashes,gaps between the letures, removes them automatically.</li>"+
+"<li> Detects double lectures and spreads them over the week.</li>"+
+"<li> Freezes particular lecture(s), if required, during auto adjustment routine.</li>"+
+"<li> Swaps Teachers or Subjects within seconds.</li>"+
+"<li> Handles Parallel Lectures and Practicals easily.</li>"+
+"<li> Optional Wizard to generate time table from scratch.</li>"+
+"<li> Prints all type of reports - Classes, Teachers and Master.</li>"+
+"<li> Excellent User Interface.</li>"+
+  "</ul>";
+    
+    
     JEditorPane editor = new JEditorPane("text/html",text);
       editor.setEditable(false);
       p1.setLayout(new BorderLayout());
